893d7172e6e34f846fb31e4c4f63c14bea8ed15e
SolalDR <solal.dussout-revel@hotmail.fr>
2021-01-26 16:19:24 +0100

    fix(esm): Add ThreeLoader in export

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,5 @@ export { Manager } from './Manager'
 export { Loader } from './Loader'
 export { Group } from './Group'
 export { File } from './File'
+
+export { ThreeLoader } from './examples/ThreeLoader' 
